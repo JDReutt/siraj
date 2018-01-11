6f365a3585fe501a652cb93aafb055078bb15f32
Jdreutt <joe.reutt@gmail.com>
2018-01-10 23:34:57 -0500

    sentiment analysis of tweets mentioning using a bag of words approach with the textblob library

---
+++ TwitterSentiment.py
@@ -0,0 +1,23 @@
+import tweepy
+from textblob import textblob
+
+#tokenize text
+#count from bag of words
+
+consumer_key = 'SixBZ6Tcnei39gy71t392bJWx'
+consumer_secret = 'KiU3B6nNytnokDXeKWX53bJBcxNHrnijzvFNTaMoejSIqfRhai'
+
+access_token = '541050252-8VXZFcGwvWjQArbkkfk28M2iLph9PCr2pcnkzz9X'
+access_token_secret = 'kzkBntEXphDfO45LTXwYnGSsuOv0nmHFCBYboJMSafJHC'
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+public_tweets = api.search('Trump')
+
+for tweet in public_tweets:
+	print(tweet.text)
+	analysis = TextBlob(tweet.text)
+	print(analysis.sentiment)
